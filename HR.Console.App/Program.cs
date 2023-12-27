743eb77e1721cc7aa1bc81c6b555bc54ec8afcc1
Ziya Memmedli <ziyarma@code.edu.az>
2023-12-27 10:59:10 +0400

    added project menu and Enum

--- HR.Console.App/Program.cs
+++ HR.Console.App/Program.cs
@@ -1,2 +1,42 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using HR.Business.Services;
 
+CompanyService companyService = new();
+DepartmentService departmentService = new();
+EmployeeService employeservice = new();
+string Username = "ziya";
+string Password = "zm040";
+bool control = true;
+Start:
+Console.WriteLine("Enter username:");
+string? username = Convert.ToString(Console.ReadLine());
+Console.WriteLine("Enter password");
+string? password = Convert.ToString(Console.ReadLine());
+if (username == Username && password == Password)
+{
+    Console.WriteLine("");
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Welcome to HR project\n" +
+                     "");
+    Console.ResetColor();
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Incorrect Passsword or Username\n" +
+                      "");
+    Console.ResetColor();
+    goto Start;
+}
