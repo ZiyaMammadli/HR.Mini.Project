eb35bf2564332eec2222d7e07422658b4606d6ff
Ziya Memmedli <ziyarma@code.edu.az>
2023-12-27 10:41:18 +0400

    added ChangeDepartment and Create methods

--- HR.Business/Interfaces/IEmployeeService.cs
+++ HR.Business/Interfaces/IEmployeeService.cs
@@ -6,7 +6,7 @@ public interface IEmployeeService
     public void Create(string? name, string? surname, int? salary);
     public void Deactive(int? id);
     public void ShowAll();
-    public void ChangeDepartment(int? departId, int? employeeId);
+    public void ChangeDepartment(int? departID, int? employeeId);
     public void İncreaseEmployeeSalary(int? employeeId, int? increaseSalary);
     public void DecreaseEmployeeSalary(int? employeeId, int? decreaseSalary);
 }
--- HR.Business/Services/EmployeeService.cs
+++ HR.Business/Services/EmployeeService.cs
@@ -1,18 +1,45 @@
 ﻿using System;
 using HR.Business.Interfaces;
+using HR.Business.Utilities.Exceptions;
+using HR.Core.Entities;
+using HR.DataAccess.Contexts;
 
 namespace HR.Business.Services;
 
 public class EmployeeService : IEmployeeService
 {
-    public void ChangeDepartment(int? departId, int? employeeId)
+    private IDepartmentService departmentService { get; }
+    public EmployeeService()
     {
-        throw new NotImplementedException();
+        departmentService = new DepartmentService();
+    }
+    public void ChangeDepartment(int? departID, int? employeeId)
+    {
+        if (employeeId is null) throw new ArgumentNullException();
+        Employee? Dbemploye = HRDbContext.dbEmployees.Find(c => c.Id == employeeId);
+        if (Dbemploye is null) throw new NotFoundException($"{employeeId} is not found");
+        if (departID is null) throw new ArgumentNullException();
+        Department? dbdepartment = HRDbContext.dbDepartments.Find(b => b.Id == departID);
+        if (dbdepartment is null) throw new NotFoundException($"{departID} is not found");
+        foreach (var department in HRDbContext.dbDepartments)
+        {
+            if (Dbemploye.DepartmentId == department.Id)
+            {
+                department.CurrentEmployee--;
+            }
+        }
+        Dbemploye.DepartmentId = dbdepartment.Id;
+        dbdepartment.CurrentEmployee++;
+        departmentService.ShowDepartmentDetails(departID);
     }
 
     public void Create(string? name, string? surname, int? salary)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(name)) throw new ArgumentNullException();
+        if (salary is null) throw new ArgumentNullException();
+        Employee employe = new Employee(name, surname, salary);
+        HRDbContext.dbEmployees.Add(employe);
+        employe.IsActivate = true;
     }
 
     public void Deactive(int? id)
