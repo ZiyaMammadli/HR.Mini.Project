1f1c38b008ef5b9e6b3a92f33ddea2b2616f3020
Ziya Memmedli <ziyarma@code.edu.az>
2023-12-27 11:09:05 +0400

    modified project menu

--- HR.Console.App/Program.cs
+++ HR.Console.App/Program.cs
@@ -18,20 +18,60 @@ if (username == Username && password == Password)
     Console.WriteLine("Welcome to HR project\n" +
                      "");
     Console.ResetColor();
+    while (control == true)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("<<< COMPANY >>>\n" +
+                      "1.Create new Company\n" +
+                      "2.Deactivate Company\n" +
+                      "3.Show all the departments of the your selected Company\n" +
+                      "4.Show all Companies\n" +
+                      "5.Find Company with own ID\n" +
+                      "\n" +
+                      "\n" +
+                      "<<< Department >>>\n" +
+                      "6.Create new Department\n" +
+                      "7.Deactivate Department\n" +
+                      "8.Add new Employees to your selected Department\n" +
+                      "9.Update your selected Department\n" +
+                      "10.Show Employees in your selected Department\n" +
+                      "11.Show Department details\n" +
+                      "12.Show all Departments\n" +
+                      "\n" +
+                      "\n" +
+                      "<<< Employee >>>\n" +
+                      "13.Create new Employee\n" +
+                      "14.Deactivate Employee\n" +
+                      "15.Show all Employees\n" +
+                      "16.Change the Employee's Department\n" +
+                      "17.Update Employee salary\n" +
+                      "18.Quit\n");
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("Please enter the serial number of your chosen process:");
+        Console.ResetColor();
+        string? optionn = Console.ReadLine();
+        bool checkParse = int.TryParse(optionn, out int option);
+        Console.ForegroundColor = ConsoleColor.Red;
+        if (checkParse == false) Console.WriteLine("\n" +
+                                                   "<<<< Only number should be entered. >>>>\n" +
+                                                   ""); ;
+        Console.ResetColor();
+        if (option <= 18 && option > 0)
+        {
 
 
 
 
-
-
-
-
-
-
-
-
-
-}
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Numbers from 0 to 18 (18 inclusive) must be entered.\n");
+            Console.ResetColor();
+        }
+    }
+}       
 else
 {
     Console.ForegroundColor = ConsoleColor.Red;
